13f7ceb0ba16d9ae3c40dd00cafc2b26c17c4a39
Raphael <raphael.ferreira@hetic.net>
2020-08-24 13:11:01 +0200

    start project

---
+++ src/Components/Sidebar.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Sidebar() {
+  return (
+    <div className="sidebar">
+      {/* Twitter icon */}
+
+      {/* SidebarOption */}
+      {/* SidebarOption */}
+      {/* SidebarOption */}
+      {/* SidebarOption */}
+
+      {/* Tweet Button */}
+    </div>
+  );
+}
+
+export default Sidebar;
