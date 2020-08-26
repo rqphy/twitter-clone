b5db60943edf11c539cb24a9153d40f2f650d725
Raphael <raphael.ferreira@hetic.net>
2020-08-26 17:11:21 +0200

    use twitter-embed

--- src/Components/Widgets.js
+++ src/Components/Widgets.js
@@ -1,10 +1,25 @@
 import React from "react";
 import "../styles/Widgets.css";
+import SearchIcon from "@material-ui/icons/Search";
+import { TwitterTweetEmbed, TwitterTimelineEmbed } from "react-twitter-embed";
 
 function Widgets() {
   return (
     <section className="widgets">
-      <h2>Widgets</h2>
+      <div className="widgets__input">
+        <SearchIcon className="widgets__searchIcon" />
+        <input palceholder="Search Twitter" />
+      </div>
+      <div className="widgets__widgetContainer">
+        <h2>What's happening</h2>
+
+        <TwitterTweetEmbed tweetId={"1292927001825808385"} />
+        <TwitterTimelineEmbed
+          sourceType="profile"
+          screenName="reactjs"
+          options={{ height: 400 }}
+        />
+      </div>
     </section>
   );
 }
